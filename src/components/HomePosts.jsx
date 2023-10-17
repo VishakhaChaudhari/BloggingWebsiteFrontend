5b96a11f7944ed3270fc722c655b9938934b5f81
VishakhaChaudhari <vishakhachaudhari04@gmail.com>
2023-10-17 10:53:09 +0530

    Initial Commit

--- src/components/HomePosts.jsx
+++ src/components/HomePosts.jsx
@@ -4,14 +4,14 @@ import {IF} from '../url'
 
 const HomePosts = ({post}) => {
   return (
-    <div className="w-full flex mt-8 space-x-4 border-2 border-solid border-gray-300 rounded-lg p-5">
+    <div className="w-full flex mt-8 space-x-4">
     {/* left */}
     <div className="w-[35%] h-[200px] flex justify-center items-center">
-    <img src={IF+post.photo} alt="" className="h-full w-full object-cover "/>
+    <img src={IF+post.photo} alt="" className="h-full w-full object-cover"/>
     </div>
     {/* right */}
     <div className="flex flex-col w-[65%]">
-      <h1 className="text-l font-bold md:mb-2 mb-1 md:text-2xl">
+      <h1 className="text-xl font-bold md:mb-2 mb-1 md:text-2xl">
       {post.title}
       </h1>
       <div className="flex mb-2 text-sm font-semibold text-gray-500 items-center justify-between md:mb-4">
@@ -21,15 +21,11 @@ const HomePosts = ({post}) => {
        <p>{new Date(post.updatedAt).toString().slice(16,24)}</p>
        </div>
       </div>
-      {/* <p >{post.desc.slice(0,200)+" ...Read more"}</p> */}
-      <div
-      className="text-sm md:text-lg" dangerouslySetInnerHTML={{__html:post.desc.slice(0,200)+" ...Read more"}}
-      />
-      </div>
+      <p className="text-sm md:text-lg">{post.desc.slice(0,200)+" ...Read more"}</p>
+    </div>
 
     </div>
   )
 }
 
 export default HomePosts
-
--- src/url.js
+++ src/url.js
@@ -1,5 +1,5 @@
-// export const URL="http://localhost:5000"
-// export const IF="http://localhost:5000/images/"
+export const URL="http://localhost:5000"
+export const IF="http://localhost:5000/images/"
 
-export const URL="https://blomarket.onrender.com"
-export const IF="https://blomarket.onrender.com/images/"
+// export const URL="https://blomarket.onrender.com"
+// export const IF="https://blomarket.onrender.com/images/"
